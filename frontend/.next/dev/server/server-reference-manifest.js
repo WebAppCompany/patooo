878582e6960611b232f34f49c38eb48cffe93d06
Patrissimo2 <patrik.klimo01@outlook.sk>
2026-04-06 22:26:59 +0200

    Remove .next from Git tracking

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6ppdO8ziFiA3RFqlCXvtOVQO4px3kYvjqquR1ZVmkys=\"\n}"
--- frontend/.next/dev/static/chunks/app_login_page_tsx_12uwdpq._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_01mjgtl._.js","static/chunks/app_login_page_module_0ohb901.css"],
-    source: "dynamic"
-});
